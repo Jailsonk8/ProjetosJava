db0367e9336e92dc67b45f5d5c7f98648f8321a1
Jailsonk8 <jailsonk8@gmail.com>
2019-04-20 19:06:54 -0300

    update project java basic

---
+++ Vetor01/src/vetor01/Vetor01.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vetor01;
+
+/**
+ *
+ * @author jailson
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int n[] = new int[4];
+        //int[] n = new int[4];
+        int n[] = {3,2,8,7,5,4}; // é um objeto
+        
+        System.out.println("Total de casa de N é " + n.length);
+        for(int c = 0; c < n.length; c++){
+            System.out.println("Na posição " + c + " temos " + n[c]);
+        }
+    }
+    
+}
